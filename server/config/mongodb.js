e7f61ef445db8210aa31177fd632f1ba04ec80a5
Marmik <marmikdave08@gmail.com>
2024-10-22 16:14:26 +0530

    mongodb update

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,6 +4,6 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () =>
     console.log("Database Connected Successfully")
   );
-  await mongoose.connect(`${process.env.MONGODB_URI}`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
 };
 export default connectDB;
